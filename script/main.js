b8563cce6b89d5feecc2052551c9a203b082b224
root <root@localhost>
2022-02-16 15:21:40 +0530

    publishing awesome pages 1645005100387

---
+++ script/main.js
@@ -0,0 +1,47 @@
+var name,email,phone,say,link,menu;
+
+
+
+function send(){ 
+ /*   name = document.getElementById("name").value; 
+    email = document.getElementById("email").value; 
+    phone = document.getElementById("phone").value; 
+    say = document.getElementById("say").value; 
+    Subject = name+"%0d%0aEmail: "+email+"%0d%0aContact No.: "+phone; 
+ */       Email.send({ 
+        Host: "smtp.gmail.com", 
+        Username: "goodjobprem@gmail.com", 
+        Password: "17112004wed", 
+        To: 'prem12937@gmail.com', 
+        From: "goodjobprem@gmail.com", 
+        Subject: "Sending Email using javascript", 
+        Body: "Well that was easy!!", 
+      }) 
+        .then(function (message) { 
+          alert("mail sent successfully") 
+        }); 
+}
+
+function shortsend(){ 
+    say = document.getElementById("say").value; 
+    link = "mailto:prem3rdac@gmail.com?body="+say; 
+    location.href = link; 
+}
+
+function openmenu(){
+    menu = document.getElementById("menu-child").style;
+    if(menu.height == "200vw"){
+        menu.height = "0vw";
+        menu.width = "0vw";
+        menu.bottom = "100vh";
+        menu.right = "0vw";
+        menu.color = "rgba(0,0,0,0)";
+    }
+    else{
+        menu.height = "200vw";
+        menu.width = "200vw";
+        menu.bottom = "30vh";
+        menu.right = "-75vw";
+        menu.color = "rgba(0,0,0,.7)";
+    }
+}
